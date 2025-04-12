9c5051a1552c2837764103ccaec62c7279fdaba0
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2025-04-12 21:06:21 +0200

    Add Gamepad Inputs and fix my gamepad
    For reasons unknown, Godot reports a different GUID and mapping than
    SDL2. This causes all inputs to go nuts. The Autoload runs once on
    startup and adds fixed mappings but with the Godot GUID instead of the
    SDL2 one.

---
+++ Tower Defense/Scripts/Gamepad/GamepadFix.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class GamepadFix : Node
+{
+	public override void _Ready()
+	{
+		Input.AddJoyMapping(
+			"03000000373500004010000000010000,Generic X-Box pad,a:b0,b:b1,x:b2,y:b3,back:b6,guide:b8,start:b7,leftstick:b9,rightstick:b10,leftshoulder:b4,rightshoulder:b5,dpup:h0.1,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,leftx:a0,lefty:a1,rightx:a3,righty:a4,lefttrigger:a2,righttrigger:a5,crc:6586,platform:Linux,",
+			true
+		);
+	}
+}
