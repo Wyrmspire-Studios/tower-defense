46f7ecbd41f23e13a3b291f4587ac242e6151d77
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2025-04-12 17:42:39 +0200

    Add MainCamera movement

--- Tower Defense/Scripts/Camera/MainCamera.cs
+++ Tower Defense/Scripts/Camera/MainCamera.cs
@@ -1,8 +1,14 @@
 using Godot;
 using System;
 
+namespace WyrmspireStudios;
 public partial class MainCamera : Camera2D
 {
+	/// <summary>
+	/// Movement speed of the camera
+	/// </summary>
+	[Export] private float _movementSpeed = 250f;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +17,14 @@ public partial class MainCamera : Camera2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var movement = Vector2.Zero;
+		if (Input.IsActionPressed("Move Up")) movement += Vector2.Up;
+		if (Input.IsActionPressed("Move Down")) movement += Vector2.Down;
+		if (Input.IsActionPressed("Move Left")) movement += Vector2.Left;
+		if (Input.IsActionPressed("Move Right")) movement += Vector2.Right;
+
+		movement = movement.Normalized();
+
+		Position += movement * (_movementSpeed * (float)delta);
 	}
 }
