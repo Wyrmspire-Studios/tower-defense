de0d29169855f570578426a37524c84345f80fe5
TheAfky <102458483+TheAfky@users.noreply.github.com>
2025-05-14 09:20:31 +0200

    Updated GameData class and testing

--- Tower Defense/Scripts/GameData.cs
+++ Tower Defense/Scripts/GameData.cs
@@ -3,26 +3,51 @@ namespace WyrmspireStudios;
 public static class GameData
 {
     public delegate void ValueChangeHandler(int oldValue, int newValue);
+
     public static event ValueChangeHandler HealthChanged;
     public static event ValueChangeHandler GoldChanged;
     public static event ValueChangeHandler ShardsChanged;
-    
-    public static int Health { get; private set; } = 100;
-    public static int Gold { get; private set; }
-    public static int Shards {get; private set;}
 
+    private static int Health { get; set; } = 100;
+    private static int Gold { get; set; }
+    private static int Shards { get; set; }
+
+    public static int GetHealth()
+    {
+        return Health;
+    }
+
+    public static void SetHealth(int amount)
+    {
+        var oldHealth = Health;
+        Health = amount;
+        HealthChanged?.Invoke(oldHealth, Health);
+    }
+    
     public static void AddHealth(int amount)
     {
         var oldHealth = Health;
         Health += amount;
         HealthChanged?.Invoke(oldHealth, Health);
     }
-    
+
     public static void RemoveHealth(int amount)
     {
         var oldHealth = Health;
         Health -= amount;
         HealthChanged?.Invoke(oldHealth, Health);
+    }
+
+    public static int GetGold()
+    {
+        return Gold;
+    }
+
+    public static void SetGold(int amount)
+    {
+        var oldGold = Gold;
+        Gold = amount;
+        GoldChanged?.Invoke(oldGold, Gold);
     }
     
     public static void AddGold(int amount)
@@ -31,7 +56,7 @@ public static class GameData
         Gold += amount;
         GoldChanged?.Invoke(oldGold, Gold);
     }
-    
+
     public static void RemoveGold(int amount)
     {
         var oldGold = Gold;
@@ -39,14 +64,25 @@ public static class GameData
         GoldChanged?.Invoke(oldGold, Gold);
     }
     
+    public static int GetShards()
+    {
+        return Shards;
+    }
+
+    public static void SetShards(int amount)
+    {
+        var oldShards = Shards;
+        Shards = amount;
+        ShardsChanged?.Invoke(oldShards, Shards);
+    }
+
     public static void AddShards(int amount)
     {
         var oldShards = Shards;
         Shards += amount;
         ShardsChanged?.Invoke(oldShards, Shards);
-        
     }
-    
+
     public static void RemoveShards(int amount)
     {
         var oldShards = Shards;
--- Tower Defense/Scripts/GameDataTesting.cs
+++ Tower Defense/Scripts/GameDataTesting.cs
@@ -20,16 +20,22 @@ public partial class GameDataTesting : GridContainer
 		addHealthButton.Pressed += () => GameData.AddHealth(1);
 		var removeHealthButton = GetNode<Button>("RemoveHealthButton");
 		removeHealthButton.Pressed += () => GameData.RemoveHealth(1);
+		var resetHealthButton = GetNode<Button>("ResetHealthButton");
+		resetHealthButton.Pressed += () => GameData.SetHealth(100);
 
 		var addGoldButton = GetNode<Button>("AddGoldButton");
 		addGoldButton.Pressed += () => GameData.AddGold(1);
 		var removeGoldButton = GetNode<Button>("RemoveGoldButton");
 		removeGoldButton.Pressed += () => GameData.RemoveGold(1);
+		var resetGoldButton = GetNode<Button>("ResetGoldButton");
+		resetGoldButton.Pressed += () => GameData.SetGold(0);
 
 		var addShardsButton = GetNode<Button>("AddShardsButton");
 		addShardsButton.Pressed += () => GameData.AddShards(1);
 		var removeShardsButton = GetNode<Button>("RemoveShardsButton");
 		removeShardsButton.Pressed += () => GameData.RemoveShards(1);
+		var resetShardsButton = GetNode<Button>("ResetShardsButton");
+		resetShardsButton.Pressed += () => GameData.SetShards(0);
 	}
 
 	private void UpdateHealthLabel(int oldValue, int newValue)
@@ -39,7 +45,7 @@ public partial class GameDataTesting : GridContainer
 	
 	private void UpdateGoldLabel(int oldValue, int newValue)
 	{
-		_goldLabel.Text = $"Health: {newValue}";
+		_goldLabel.Text = $"Gold: {newValue}";
 	}
 	
 	private void UpdateShardsLabel(int oldValue, int newValue)
