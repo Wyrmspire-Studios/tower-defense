9cabff63b96caa21998e9ecc2c3f6ae14998f0c7
theRacterr <197937175+theRacterr@users.noreply.github.com>
2025-05-14 20:16:48 +0200

    Slime basics
    Slime scene and basis for a script

---
+++ Tower Defense/Scripts/Enemies/Slime.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class Slime : PathFollow2D
+{
+	private int _speed = 100;
+	[Export] private int _health = 100;
+	[Export] private int _coinsDropped = 1;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		SetProgress((float)(GetProgress() + _speed * delta));
+	}
+}
